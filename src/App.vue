f867353176bc1edc9817f251384d4c2029cd3dc0
unknown <merufrancis21@gmail.com>
2022-11-25 03:26:45 -0800

    vue-todo-app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <todo-app></todo-app>
+  </div>
+</template>
+
+<script>
+import TodoApp from './components/TodoApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoApp
+  }
+}
+</script>
+
+<style scoped>
+
+body {
+  font-family: 'Poppins', sans-serif;
+  background-color: rgb(29, 29, 31);
+}
+.app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
